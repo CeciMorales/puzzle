913b3cd3b50e154eec4907bbd6149d4f0e676025
Morales Arriaga <c.morales.arriaga@accenture.com>
2021-06-28 13:56:02 -0500

    improved input instant search

--- libs/books/feature/src/lib/book-search/book-search.component.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.ts
@@ -7,7 +7,7 @@ import {
   ReadingListBook,
   searchBooks
 } from '@tmo/books/data-access';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Book } from '@tmo/shared/models';
 import { Observable } from 'rxjs';
 import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
@@ -20,49 +20,41 @@ import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 export class BookSearchComponent implements OnInit {
   books$: Observable<ReadingListBook[]> ;
 
-  searchForm: FormGroup;
-  term = '';
+  term = new FormControl('');
 
   constructor(
     private readonly store: Store,
-    private readonly fb: FormBuilder,
     
   ) {
     this.books$ = this.store.select(getAllBooks)
+
+    this.term.valueChanges
+    .pipe(
+      debounceTime(500),
+      distinctUntilChanged()
+    )
+    .subscribe((value) => this.searchBooks())
+  
   }
 
   get searchTerm(): string {
-    return this.searchForm.value.term
+    return this.term.value
   }
 
   ngOnInit(): void {
     
-    this.searchForm = this.fb.group({
-        term: [this.term]
-      });
-  
-      this.searchForm.controls['term']
-        .valueChanges
-        .pipe(
-          debounceTime(500),
-          distinctUntilChanged()
-        )
-        .subscribe((value) => this.searchBooks())
-    
   }
 
   searchExample() {
-    this.searchForm.controls.term.setValue('javascript');
+    this.term.setValue('javascript');
     this.searchBooks();
   }
 
   searchBooks() {
-
-    if (this.term) {
+    if ( this.searchTerm ) {
       this.store.dispatch(searchBooks({ term: this.searchTerm }));
     } else {
       this.store.dispatch(clearSearch());
     }
   }
-
 }
